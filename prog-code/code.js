c13ea2757b4cf4c0bdfbd28f214ebec9cdcee1c2
kkmin1 <kkmin1@naver.com>
2018-02-16 09:33:17 +0900

    Add files via upload

---
+++ prog-code/code.js
@@ -0,0 +1,78 @@
+$(function(){
+    $("p#c1").click(function(){
+        $("#center").load("code.txt pre#c1");
+    });
+    $("p#c2").click(function(){
+        $("#center").load("code.txt pre#c2");
+    });
+    $("p#cp1").click(function(){
+        $("#center").load("code.txt pre#cp1");
+    });
+    $("p#cp2").click(function(){
+        $("#center").load("code.txt pre#cp2");
+    });
+    $("p#cs1").click(function(){
+        $("#center").load("code.txt pre#cs1");
+    });
+    $("p#cs2").click(function(){
+        $("#center").load("code.txt pre#cs2");
+    });
+    $("p#j1").click(function(){
+        $("#center").load("code.txt pre#j1");
+    });
+    $("p#j2").click(function(){
+        $("#center").load("code.txt pre#j2");
+    });
+    $("p#py1").click(function(){
+        $("#center").load("code.txt pre#py1");
+    });
+    $("p#pl1").click(function(){
+        $("#center").load("code.txt pre#pl1");
+    });
+    $("p#r1").click(function(){
+        $("#center").load("code.txt pre#r1");
+    });
+    $("p#h1").click(function(){
+        $("#center").load("code.txt pre#h1");
+    });
+    $("p#ph1").click(function(){
+        $("#center").load("code.txt pre#ph1");
+    });
+    $("p#js1").click(function(){
+        $("#center").load("code.txt pre#js1");
+    });
+    $("p#jq1").click(function(){
+        $("#center").load("code.txt pre#jq1");
+    });
+    $("p#k1").click(function(){
+        $("#center").load("code.txt pre#k1");
+    });
+    $("p#s1").click(function(){
+        $("#center").load("code.txt pre#s1");
+    });
+
+    $("p#l1").click(function(){
+       $("#center").load("code.txt pre#l1");
+    });
+
+    $("p#jl1").click(function(){
+       $("#center").load("code.txt pre#jl1");
+    });
+
+    $("p#g1").click(function(){
+       $("#center").load("code.txt pre#g1");
+    });
+    $("p#rs1").click(function(){
+       $("#center").load("code.txt pre#rs1");
+    });
+    $("p#ex1").click(function(){
+       $("#center").load("code.txt pre#ex1");
+    });
+    $("p#cl1").click(function(){
+       $("#center").load("code.txt pre#cl1");
+    });
+    $("p#ns1").click(function(){
+       $("#center").load("code.txt pre#ns1");
+    });
+
+});
---
+++ prog-code/test.js
@@ -0,0 +1,27 @@
+var arr =
+[
+   ["c1", "c2", "c3", "c4", "c5","c6", "c7"],
+   ["cp1", "cp2", "cp3", "cp4", "cp5", "cp6", "cp7"],
+   ["cs1", "cs2", "cs3", "cs4", "cs5", "cs6", "cs7"],
+   ["g1", "g2", "g3", "g4", "g5", "g6", "g7"],
+   ["j1", "j2", "j3", "j4", "j5", "j6", "j7"],
+   ["py1", "py2", "py3", "py4", "py5", "py6", "py7"],
+   ["pl1", "pl2", "pl3", "pl4", "pl5", "pl6", "pl7"]
+];
+
+var fname = "test.txt";
+var p = "p";
+var pre = "pre";
+var sharp = "#";
+var div = "div";
+var sp = " ";
+var sel = div+sharp+arr[0][0];
+var url = fname+sp+p+sharp+arr[0][0];
+var txt = "";
+
+txt += "<select>"
+for (x in myObj) {
+    txt += "<option>" + myObj[x].name;
+}
+txt += "</select>"
+document.getElementById("demo").innerHTML = txt;
