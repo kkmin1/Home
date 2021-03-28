ecdc9b15ce3990b802434d860820c2244cb6ac1d
kkmin1 <kkmin1@naver.com>
2021-03-28 12:23:44 +0900

    Add files via upload

--- code.js
+++ code.js
@@ -31,6 +31,10 @@ $(function(){
         $("#center").load("code.txt pre#cs2");
     });
     
+    $("p#cs3").click(function(){
+        $("#center").load("code.txt pre#cs3");
+    });
+
     $("p#j1").click(function(){
         $("#center").load("code.txt pre#j1");
     });
@@ -39,6 +43,10 @@ $(function(){
         $("#center").load("code.txt pre#j2");
     });
     
+    $("p#j3").click(function(){
+        $("#center").load("code.txt pre#j3");
+    });
+
     $("p#py1").click(function(){
         $("#center").load("code.txt pre#py1");
     });
@@ -47,12 +55,20 @@ $(function(){
         $("#center").load("code.txt pre#py2");
     });
     
+    $("p#py3").click(function(){
+        $("#center").load("code.txt pre#py3");
+    });
+
     $("p#pl1").click(function(){
         $("#center").load("code.txt pre#pl1");
     });
     
     $("p#pl2").click(function(){
         $("#center").load("code.txt pre#pl2");
+    });
+
+    $("p#pl3").click(function(){
+        $("#center").load("code.txt pre#pl3");
     });
 
     $("p#r1").click(function(){
@@ -63,56 +79,165 @@ $(function(){
         $("#center").load("code.txt pre#r2");
     });
 
+    $("p#r3").click(function(){
+        $("#center").load("code.txt pre#r3");
+    });
+
     $("p#h1").click(function(){
         $("#center").load("code.txt pre#h1");
     });
     
+    $("p#h2").click(function(){
+        $("#center").load("code.txt pre#h2");
+    });
+
+    $("p#h3").click(function(){
+        $("#center").load("code.txt pre#h3");
+    });
+
     $("p#ph1").click(function(){
         $("#center").load("code.txt pre#ph1");
     });
     
+    $("p#ph2").click(function(){
+        $("#center").load("code.txt pre#ph2");
+    });
+
+    $("p#ph3").click(function(){
+        $("#center").load("code.txt pre#ph3");
+    });
+
     $("p#js1").click(function(){
         $("#center").load("code.txt pre#js1");
     });
     
+    $("p#js2").click(function(){
+        $("#center").load("code.txt pre#js2");
+    });
+
+    $("p#js3").click(function(){
+        $("#center").load("code.txt pre#js3");
+    });
+
     $("p#jq1").click(function(){
         $("#center").load("code.txt pre#jq1");
     });
     
+    $("p#jq2").click(function(){
+        $("#center").load("code.txt pre#jq2");
+    });
+
+    $("p#jq3").click(function(){
+        $("#center").load("code.txt pre#jq3");
+    });
+
     $("p#k1").click(function(){
         $("#center").load("code.txt pre#k1");
     });
     
+    $("p#k2").click(function(){
+        $("#center").load("code.txt pre#k2");
+    });
+
+    $("p#k3").click(function(){
+        $("#center").load("code.txt pre#k3");
+    });
+
     $("p#s1").click(function(){
         $("#center").load("code.txt pre#s1");
+    });
+
+    $("p#s2").click(function(){
+        $("#center").load("code.txt pre#s2");
+    });
+
+    $("p#s3").click(function(){
+        $("#center").load("code.txt pre#s3");
     });
 
     $("p#l1").click(function(){
        $("#center").load("code.txt pre#l1");
     });
 
+    $("p#l2").click(function(){
+        $("#center").load("code.txt pre#l2");
+    });
+
+    $("p#l3").click(function(){
+        $("#center").load("code.txt pre#l3");
+    });
+
     $("p#jl1").click(function(){
        $("#center").load("code.txt pre#jl1");
+    });
+
+    $("p#jl2").click(function(){
+        $("#center").load("code.txt pre#jl2");
+     });
+
+    $("p#jl3").click(function(){
+        $("#center").load("code.txt pre#jl3");
     });
 
     $("p#g1").click(function(){
        $("#center").load("code.txt pre#g1");
     });
     
+    $("p#g2").click(function(){
+        $("#center").load("code.txt pre#g2");
+    });
+
+    $("p#g3").click(function(){
+        $("#center").load("code.txt pre#g3");
+    });
+
     $("p#rs1").click(function(){
        $("#center").load("code.txt pre#rs1");
     });
     
+    $("p#rs2").click(function(){
+        $("#center").load("code.txt pre#rs2");
+     });
+    
+     $("p#rs3").click(function(){
+        $("#center").load("code.txt pre#rs3");
+     });
+
     $("p#ex1").click(function(){
        $("#center").load("code.txt pre#ex1");
     });
     
+    $("p#ex2").click(function(){
+        $("#center").load("code.txt pre#ex2");
+     });
+
+     $("p#ex3").click(function(){
+        $("#center").load("code.txt pre#ex3");
+     });
+
+
     $("p#cl1").click(function(){
        $("#center").load("code.txt pre#cl1");
     });
     
+    $("p#cl2").click(function(){
+        $("#center").load("code.txt pre#cl2");
+     });
+    
+     $("p#cl3").click(function(){
+        $("#center").load("code.txt pre#cl3");
+     });
+
     $("p#ns1").click(function(){
        $("#center").load("code.txt pre#ns1");
     });
 
+    $("p#ns2").click(function(){
+        $("#center").load("code.txt pre#ns2");
+     });
+    
+     $("p#ns3").click(function(){
+        $("#center").load("code.txt pre#ns3");
+     });
+    
 });
